f1a3afa293734b4798828aab55c536d4be2617c0
Lettle <1071445082@qq.com>
2023-05-18 11:14:51 +0800

    Forgot an entity

---
+++ eurekaclient/src/main/java/cn/lettle/entity/Player.java
@@ -0,0 +1,42 @@
+/**
+ * @Author: Lettle
+ * @Create: 2023-05-17 17:37
+ * @Description: 玩家类
+ **/
+package cn.lettle.entity;
+
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("player")
+public class Player {
+
+    @TableId(value="player_id", type = IdType.AUTO)
+    int id;
+
+    String username;
+    String password;
+
+    boolean isOnline;
+    boolean isBanned;
+
+    String email;
+    @TableField("serverId")
+    int serverId;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @TableField(fill = FieldFill.INSERT)
+    Date lastLoginTime;
+
+}
